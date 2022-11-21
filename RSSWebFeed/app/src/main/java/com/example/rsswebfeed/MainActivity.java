4451eade108c55469e60186825628c17b5c6b1a3
FinityKing <71716915+FinityM@users.noreply.github.com>
2022-11-21 16:10:33 +0000

    Deleted everything again
    The implementation does not work due to using external libraries

--- RSSWebFeed/app/src/main/java/com/example/rsswebfeed/MainActivity.java
+++ RSSWebFeed/app/src/main/java/com/example/rsswebfeed/MainActivity.java
@@ -10,41 +10,13 @@ import android.widget.EditText;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    ArrayList<String> rssLinks = new ArrayList<>();
-
+public class MainActivity extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnOpen = findViewById(R.id.btnOpenRssLink);
-        Button btnRediff = findViewById(R.id.btnRediff);
-        Button btnCinemaBlend = findViewById(R.id.btnCinemaBlend);
-        EditText etRssLink = findViewById(R.id.etRssLink);
-
-        btnOpen.setOnClickListener(this);
-        btnRediff.setOnClickListener(this);
-        btnCinemaBlend.setOnClickListener(this);
-
-        rssLinks.add("https://www.rediff.com/rss/moviesrss.xml");
-        rssLinks.add("https://www.cinemablend.com/rss_news.php");
     }
 
-    @Override
-    public void onClick(View view) {
-        Intent intent = new Intent(getApplicationContext(), RSSFeedActivity.class);
-        switch (view.getId()) {
-            case R.id.btnRediff:
-                intent.putExtra("rssLink", rssLinks.get(0));
-                startActivity(intent);
-                break;
-            case R.id.btnCinemaBlend:
-                intent.putExtra("rssLink", rssLinks.get(1));
-                startActivity(intent);
-                break;
-        }
-
-    }
 }
